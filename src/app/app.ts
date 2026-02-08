37259c19a50f6803e4e6c115bf595d8e44f33201
Timofey Naryshkin <naryshkint4@gmail.com>
2026-02-08 22:47:15 +0900

    feat: add basic tabswitcher component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './sidebar/sidebar';
+import { TabSwitcher } from "./tab-switcher/tab-switcher";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule, Sidebar],
+  imports: [RouterOutlet, MatSidenavModule, Sidebar, TabSwitcher],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/tab-switcher/tab-switcher.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+
+@Component({
+  selector: 'app-tab-switcher',
+  imports: [MatTabsModule],
+  templateUrl: './tab-switcher.html',
+  styleUrl: './tab-switcher.scss',
+})
+export class TabSwitcher {
+
+}
