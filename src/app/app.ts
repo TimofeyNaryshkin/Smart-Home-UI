f0c4c3bd7d28fdf25850799e1c5b374d2d95db3f
Timofey Naryshkin <naryshkint4@gmail.com>
2026-02-08 22:35:28 +0900

    feat: implement sidebar component, add app component sidenav and main content structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, Sidebar],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('smart-home');
-}
+export class App {}
