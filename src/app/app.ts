2374f34a26258af4bb6204644a025f4541d99d50
Timofey Naryshkin <naryshkint4@gmail.com>
2026-02-08 22:49:49 +0900

    feat: add basic dashboard component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,11 @@ import { Component, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './sidebar/sidebar';
-import { TabSwitcher } from "./tab-switcher/tab-switcher";
+import { Dashboard } from "./dashboard/dashboard";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule, Sidebar, TabSwitcher],
+  imports: [RouterOutlet, MatSidenavModule, Sidebar, Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
