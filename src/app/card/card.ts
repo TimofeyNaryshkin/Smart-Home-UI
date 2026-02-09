223e998ee1db954c8040038028bd42bb7366379b
Timofey Naryshkin <naryshkint4@gmail.com>
2026-02-10 02:04:47 +0900

    feat: add group device toggle and styles

--- src/app/card/card.ts
+++ src/app/card/card.ts
@@ -1,15 +1,33 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input, linkedSignal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { TCard } from '../models/card.model';
-import { Device } from "../device/device";
-import { Sensor } from "../sensor/sensor";
+import { Device } from '../device/device';
+import { Sensor } from '../sensor/sensor';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-card',
-  imports: [MatCardModule, Device, Sensor],
+  imports: [MatCardModule, Device, Sensor, MatSlideToggleModule],
   templateUrl: './card.html',
   styleUrl: './card.scss',
 })
 export class Card {
-  data = input.required<TCard>()
+  data = input.required<TCard>();
+
+  isDeviceGroup = computed(() => {
+    const devices = this.data().items.filter((i) => i.type === 'device');
+    return devices.length >= 2;
+  });
+
+  isGroupActive = linkedSignal(() => {
+    if (this.isDeviceGroup()) {
+      const devices = this.data().items.filter((i) => i.type === 'device');
+      return devices.some((d) => d.state === true);
+    }
+    return false;
+  }); 
+
+  toggleIsGroupActive() {
+    this.isGroupActive.update((val) => !val)
+  }
 }
