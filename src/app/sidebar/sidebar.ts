c0ec9643178d9a1a0200c6292245835fa46e0959
Timofey Naryshkin <naryshkint4@gmail.com>
2026-02-10 02:48:42 +0900

    feat: add sidebar styles

--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, MatIcon],
+  imports: [RouterLink, MatIcon, MatDividerModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
