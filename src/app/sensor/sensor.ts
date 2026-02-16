6404544059877d20cf30281d0ea61d603a0a9993
Timofey Naryshkin <naryshkint4@gmail.com>
2026-02-16 15:43:00 +0900

    feat: implement auth service and auth guard

--- src/app/sensor/sensor.ts
+++ src/app/sensor/sensor.ts
@@ -1,7 +1,7 @@
 import { Component, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { TSensor } from '../models/sensor.mode';
-import { FormatSensorValuePipe } from './format-sensor-value-pipe';
+import { FormatSensorValuePipe } from '../pipes/format-sensor-value-pipe';
 
 @Component({
   selector: 'app-sensor',
