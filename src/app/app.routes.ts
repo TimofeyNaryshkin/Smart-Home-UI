44816e4e0fbaf2b49e7202fbc47d9cd0374f8a13
Timofey Naryshkin <naryshkint4@gmail.com>
2026-02-16 18:31:24 +0900

    feat: update routing, add basic notfound component, move template and logic from app component to dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,19 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard').then((m) => m.Dashboard),
+    canActivate: [authGuard],
+  },
   {
     path: 'login',
     loadComponent: () => import('./login/login').then((m) => m.Login),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./not-found/not-found').then((m) => m.NotFound),
   },
 ];
--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
-import { TabSwitcher } from '../tab-switcher/tab-switcher';
+import { Component, computed, inject, linkedSignal, signal } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterOutlet } from '@angular/router';
+
+import { BreakpointObserver, Breakpoints, LayoutModule } from '@angular/cdk/layout';
+import { map } from 'rxjs';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { Sidebar } from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [TabSwitcher],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    Sidebar,
+    Dashboard,
+    LayoutModule,
+    MatIcon,
+    MatButtonModule,
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {}
+export class Dashboard {
+  breakpointObserver = inject(BreakpointObserver);
+  isLargeScreen = signal(this.breakpointObserver.isMatched(Breakpoints.Large));
+  mode = computed(() => (this.isLargeScreen() ? 'side' : 'over'));
+  isOpenned = linkedSignal(() => this.isLargeScreen());
+
+  toggleIsOpened() {
+    if (this.isOpenned()) {
+      this.isOpenned.set(false);
+    } else {
+      this.isOpenned.set(true);
+    }
+  }
+
+  layoutChanges = this.breakpointObserver
+    .observe(Breakpoints.Large)
+    .pipe(map((r) => r.matches))
+    .subscribe((result) => {
+      if (result) {
+        this.isOpenned.set(true);
+        this.isLargeScreen.set(true);
+      } else {
+        this.isOpenned.set(false);
+        this.isLargeScreen.set(false);
+      }
+    });
+}
