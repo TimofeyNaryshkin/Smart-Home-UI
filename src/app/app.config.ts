e7fd6ff35b5a5e83afd07ac990d778a8a531b90c
Timofey Naryshkin <naryshkint4@gmail.com>
2026-02-28 15:33:42 +0900

    feat: install ngrx store and effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,15 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './interceptors/token-interceptor';
 import { httpInterceptor } from './interceptors/http-interceptor';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([tokenInterceptor, httpInterceptor])),
-  ],
+    provideStore(),
+    provideEffects()
+],
 };
